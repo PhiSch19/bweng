78cd5f1844b0f016b4a6681516681b148155cb10
Kristijan Kusic <wi21b013@technikum-wien.at>
2023-10-24 18:40:41 +0200

    Add patch and put for room.

--- src/main/java/at/technikum/bweng/controller/RoomController.java
+++ src/main/java/at/technikum/bweng/controller/RoomController.java
@@ -6,11 +6,7 @@ import at.technikum.bweng.dto.mapper.RoomDtoMapper;
 import at.technikum.bweng.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -39,5 +35,24 @@ public class RoomController {
             throw new IllegalArgumentException("Update not allowed!");
         }
         return dtoMapper.from(roomService.addRoom(dtoMapper.from(room)));
+    }
+
+    @PatchMapping("/rooms/{id}")
+    public RoomDto patchRoom(@PathVariable UUID id, @RequestBody RoomDto room) {
+        // Check if the room exists, and if not, return an error or handle it as needed.
+        if (room.id() != null) {
+            throw new IllegalArgumentException("Patch not allowed!");
+        }
+
+        return dtoMapper.from(roomService.patchRoom(id, dtoMapper.from(room)));
+    }
+
+    @PutMapping("/rooms/{id}")
+    public RoomDto putRoom(@PathVariable UUID id, @RequestBody RoomDto room) {
+        if (room.id() != null) {
+            throw new IllegalArgumentException("Put not allowed!");
+        }
+
+        return dtoMapper.from(roomService.updateRoom(id, dtoMapper.from(room)));
     }
 }
--- src/main/java/at/technikum/bweng/service/RoomService.java
+++ src/main/java/at/technikum/bweng/service/RoomService.java
@@ -6,6 +6,7 @@ import at.technikum.bweng.repository.RoomRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -30,6 +31,35 @@ public class RoomService {
         return roomRepository.save(room);
     }
 
+    public Room patchRoom(UUID id, Room updatedRoom) {
+        Optional<Room> existingRoomOptional = roomRepository.findById(id);
 
+        if (existingRoomOptional.isPresent()) {
+            Room existingRoom = existingRoomOptional.get();
 
+            if (updatedRoom.getName() != null) {
+                existingRoom.setName(updatedRoom.getName());
+            }
+            if (updatedRoom.getCapacity() != null) {
+                existingRoom.setCapacity(updatedRoom.getCapacity());
+            }
+            if (updatedRoom.getCleaningMinutes() != null) {
+                existingRoom.setCleaningMinutes(updatedRoom.getCleaningMinutes());
+            }
+
+            return roomRepository.save(existingRoom);
+        } else {
+            return null;
+        }
+    }
+
+    public Room updateRoom(UUID id, Room updatedRoom) {
+        Optional<Room> existingRoomOptional = roomRepository.findById(id);
+
+        if (existingRoomOptional.isPresent()) {
+            return roomRepository.save(updatedRoom);
+        } else {
+            return null;
+        }
+    }
 }
