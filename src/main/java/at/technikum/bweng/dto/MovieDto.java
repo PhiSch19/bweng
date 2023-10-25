fd666590060fa6df4018e6e4adec8b94c60a6e34
Kristijan Kusic <wi21b013@technikum-wien.at>
2023-10-25 18:17:40 +0200

    Improve movie dto.

--- src/main/java/at/technikum/bweng/dto/MovieDto.java
+++ src/main/java/at/technikum/bweng/dto/MovieDto.java
@@ -1,11 +1,11 @@
 package at.technikum.bweng.dto;
 
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.util.UUID;
 
-public record MovieDto(UUID id, @NotEmpty String name, @NotNull @Positive @Max(360) Integer durationMinutes) {
+public record MovieDto(UUID id, @NotBlank String name, @NotNull @Positive @Max(360) Integer durationMinutes) {
 }
