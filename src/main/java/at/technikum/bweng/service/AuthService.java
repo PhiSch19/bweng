cdbd587fecb830de0cea361673c84d92d2c366e3
Philipp Schermann <PhiSch19@icloud.com>
2023-12-03 11:54:38 +0100

    add minio

--- src/main/java/at/technikum/bweng/service/AuthService.java
+++ src/main/java/at/technikum/bweng/service/AuthService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
 
-
     private final TokenIssuer tokenIssuer;
     private final AuthenticationManager authenticationManager;
 
