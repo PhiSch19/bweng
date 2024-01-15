8dea927278f942bd08ab1d1241d1da5a8a4fa7f3
petz1209 <petz.leitner@gmx.at>
2024-01-15 18:52:20 +0100

    user tests complete

--- src/test/java/at/technikum/bweng/controller/UserControllerTest.java
+++ src/test/java/at/technikum/bweng/controller/UserControllerTest.java
@@ -1,29 +1,26 @@
 package at.technikum.bweng.controller;
 
+import at.technikum.bweng.dto.MovieDto;
 import at.technikum.bweng.dto.UserCredentialsDto;
 import at.technikum.bweng.dto.UserDto;
-import at.technikum.bweng.entity.Movie;
 import at.technikum.bweng.entity.User;
 import at.technikum.bweng.repository.UserRepository;
 import at.technikum.bweng.service.AuthService;
 import at.technikum.bweng.service.UserService;
-import org.junit.Before;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.hamcrest.CoreMatchers.is;
-import java.util.ArrayList;
-import java.util.Optional;
+
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -84,6 +81,45 @@ class UserControllerTest {
                 );
 
     }
+
+    @Test
+    void registerExistingUser() throws Exception{
+
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("AAAAaaaa123!!!");
+        user.setFirstname("fn");
+        user.setLastname("ln");
+        user.setEmail("xxl@xx.at");
+        user.setCountry("AT");
+        user.setSalutation("sal");
+
+        userService.register(user);
+
+        Assertions.assertThatExceptionOfType(jakarta.servlet.ServletException.class)
+                        .isThrownBy(() -> mvc.perform(
+                                        MockMvcRequestBuilders.post("/user/register")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content("""
+                                        {
+                                          "username": "test",
+                                          "password": "AAAAaaaa123!!!",
+                                          "details": {
+                                            "firstname": "fn",
+                                            "lastname": "ln",
+                                            "email": "xxl@xx.at",
+                                            "country": "AT",
+                                            "salutation": "sal"
+                                          }
+                                        }
+                                        """)
+                                )
+                                .andExpect(MockMvcResultMatchers.status().isOk()));
+
+
+
+    }
+
 
     @Test
     void token() throws Exception {
@@ -243,6 +279,130 @@ class UserControllerTest {
     }
 
     @Test
+    void testPatchDetailsLastName() throws Exception{
+        /*tests if users can update their details*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "us";
+        String patched = "patched";
+
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.patch("/user/"+ user.getId() +"/details")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+                .content(String.format("""
+                                        {"lastname": "%s"}
+                                        """, patched))
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.lastname").value(patched));
+    }
+
+    @Test
+    void testPatchDetailsEmail() throws Exception{
+        /*tests if users can update their details*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "us";
+        String patched = "patched@mail.com";
+
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.patch("/user/"+ user.getId() +"/details")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+                .content(String.format("""
+                                        {"email": "%s"}
+                                        """, patched))
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.email").value(patched));
+    }
+
+    @Test
+    void testPatchDetailsCountry() throws Exception{
+        /*tests if users can update their details*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "us";
+        String patched = "US";
+
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.patch("/user/"+ user.getId() +"/details")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+                .content(String.format("""
+                                        {"country": "%s"}
+                                        """, patched))
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.country").value(patched));
+    }
+
+    @Test
+    void testPatchDetailsSalutation() throws Exception{
+        /*tests if users can update their details*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "us";
+        String patched = "other";
+
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.patch("/user/"+ user.getId() +"/details")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+                .content(String.format("""
+                                        {"salutation": "%s"}
+                                        """, patched))
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.salutation").value(patched));
+    }
+
+    @Test
     void getAllUsersAdmin() throws Exception{
         /*tests if an admin user can fetch a list of all users*/
         String password = "AAAAaaaa123!!!";
@@ -371,4 +531,110 @@ class UserControllerTest {
         response.andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.role").value(newRole));
     }
+
+    @Test
+    void uploadProfilePicture() throws Exception{
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file", "profileImage.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "This is a test".getBytes()
+        );
+
+        String password = "AAAAaaaa123!!!";
+        User user = new User();
+        user.setUsername("us");
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response = mvc.perform(MockMvcRequestBuilders.multipart("/user/"+ user.getId() +"/profile-picture")
+                        .file(file)
+                        .header("Authorization", "bearer " + token));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.profilePictureId").isNotEmpty());
+
+
+    }
+
+    @Test
+    void uploadProfilePictureNoToken() throws Exception{
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file", "profileImage.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "This is a test".getBytes()
+        );
+
+        String password = "AAAAaaaa123!!!";
+        User user = new User();
+        user.setUsername("us");
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+
+        ResultActions response = mvc.perform(MockMvcRequestBuilders.multipart("/user/"+ user.getId() +"/profile-picture")
+                .file(file));
+        response.andExpect(MockMvcResultMatchers.status().isUnauthorized());
+
+
+    }
+
+
+    @Test
+    void uploadProfilePictureOfOtherUser() throws Exception{
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file", "profileImage.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "This is a test".getBytes()
+        );
+
+        String password = "AAAAaaaa123!!!";
+        User user = new User();
+        user.setUsername("us");
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        User other = new User();
+        other.setUsername("other");
+        other.setPassword(password);
+        other.setLastname("other");
+        other.setFirstname("other");
+        other.setEmail("other@mail.com");
+        other.setCountry("AT");
+        other.setSalutation("mrs.");
+
+
+
+        userService.register(user);
+        userService.register(other);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response = mvc.perform(MockMvcRequestBuilders.multipart("/user/"+ other.getId() +"/profile-picture")
+                .file(file)
+                .header("Authorization", "bearer " + token));
+
+        response.andExpect(MockMvcResultMatchers.status().isForbidden());
+
+
+    }
+
+
 }
