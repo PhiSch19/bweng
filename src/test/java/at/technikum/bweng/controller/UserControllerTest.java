98c37e31bc388a5a0b5d6c1ba3087d74e7aa5323
petz1209 <petz.leitner@gmx.at>
2024-01-15 17:15:17 +0100

    added user controller tests

--- src/test/java/at/technikum/bweng/controller/UserControllerTest.java
+++ src/test/java/at/technikum/bweng/controller/UserControllerTest.java
@@ -2,16 +2,31 @@ package at.technikum.bweng.controller;
 
 import at.technikum.bweng.dto.UserCredentialsDto;
 import at.technikum.bweng.dto.UserDto;
+import at.technikum.bweng.entity.Movie;
+import at.technikum.bweng.entity.User;
+import at.technikum.bweng.repository.UserRepository;
+import at.technikum.bweng.service.AuthService;
+import at.technikum.bweng.service.UserService;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.CoreMatchers.is;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
@@ -24,6 +39,20 @@ class UserControllerTest {
 
     @Autowired
     private UserController userController;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeEach
+    void setup() {
+        userRepository.deleteAll();
+    }
 
     @Test
     void register() throws Exception {
@@ -80,22 +109,266 @@ class UserControllerTest {
     }
 
     @Test
-    void details() {
+    void tokenInvalidCreds() throws Exception {
+        /*Tests wether invalid credentials lead to a 401 response.*/
+        String user = "us";
+        String pw = "AAAAaaaa123!!!";
+        userController.register(
+                new UserDto(
+                        new UserCredentialsDto(user, pw),
+                        null)
+        );
+
+        mvc.perform(MockMvcRequestBuilders.post("/user/token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "username": "us",
+                                  "password": "pw"
+                                }
+                                """)
+                )
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+
+    @Test
+    void testGetDetails() throws Exception{
+        /*Tests if a user is can access details of another user.*/
+        String password = "AAAAaaaa123!!!";
+        User user = new User();
+        user.setUsername("us");
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.get("/user/"+ user.getId() +"/details")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "bearer " + token));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.firstname").value(user.getFirstname()));
     }
 
     @Test
-    void testDetails() {
+    void testGetDetailsOtherUser() throws Exception{
+        /*Tests if a user is forbidden to access details of another user.*/
+        String password = "AAAAaaaa123!!!";
+        User user = new User();
+        user.setUsername("us");
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        User other = new User();
+        other.setUsername("other");
+        other.setPassword(password);
+        other.setLastname("other");
+        other.setFirstname("other");
+        other.setEmail("other@mail.com");
+        other.setCountry("AT");
+        other.setSalutation("mrs.");
+
+        userService.register(user);
+        userService.register(other);
+
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.get("/user/"+ other.getId() +"/details")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token));
+
+        response.andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+    @Test
+    void testGetDetailsNoToken() throws Exception{
+        /*test wether the access of details is prevented for users that don't provide a token*/
+        String password = "AAAAaaaa123!!!";
+        User user = new User();
+        user.setUsername("us");
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.get("/user/"+ user.getId() +"/details")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isUnauthorized());
+
     }
 
     @Test
-    void uploadProfilePicture() {
+    void testPatchDetails() throws Exception{
+        /*tests if users can update their details*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "us";
+        String patchFirstName = "patched";
+
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.patch("/user/"+ user.getId() +"/details")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+                .content(String.format("""
+                                        {"firstname": "%s"}
+                                        """, patchFirstName))
+                );
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.firstname").value(patchFirstName));
     }
 
     @Test
-    void getAllUsersWithRole() {
-    }
+    void getAllUsersAdmin() throws Exception{
+        /*tests if an admin user can fetch a list of all users*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "admin";
 
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        user.setRole("ROLE_ADMIN");
+        userRepository.save(user);
+
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.get("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.size()").value(1)
+                );
+
+
+    }
     @Test
-    void changeRole() {
+    void getAllUsersNoToken() throws Exception{
+        /*tests is all a list of users can be fetched without a valid token*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "admin";
+
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+        user.setRole("ROLE_ADMIN");
+        userRepository.save(user);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.get("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isUnauthorized());
+
+
+    }
+    @Test
+    void getAllUsersWithoutAdminRights() throws Exception{
+        /*tests if a user without admin rights can fetch a list of all users*/
+        String password = "AAAAaaaa123!!!";
+        String initFirstName = "admin";
+
+        User user = new User();
+        user.setUsername(initFirstName);
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        userService.register(user);
+
+
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.get("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isForbidden());
+
+    }
+    @Test
+    void changeRole() throws Exception{
+        /*Tests if a user is forbidden to access details of another user.*/
+        String newRole = "ROLE_STAFF";
+
+        String password = "AAAAaaaa123!!!";
+        User user = new User();
+        user.setUsername("us");
+        user.setPassword(password);
+        user.setLastname("ln");
+        user.setFirstname("fn");
+        user.setEmail("user@mail.com");
+        user.setCountry("AT");
+        user.setSalutation("mrs.");
+
+        User other = new User();
+        other.setUsername("other");
+        other.setPassword(password);
+        other.setLastname("other");
+        other.setFirstname("other");
+        other.setEmail("other@mail.com");
+        other.setCountry("AT");
+        other.setSalutation("mrs.");
+
+        userService.register(user);
+        userService.register(other);
+
+        user.setRole("ROLE_ADMIN");
+        userRepository.save(user);
+
+        String token = authService.authenticate(user.getUsername(), password);
+
+        ResultActions response  = mvc.perform(MockMvcRequestBuilders.post("/user/"+ other.getId() +"/role")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer " + token)
+                .content(newRole));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.role").value(newRole));
     }
 }
